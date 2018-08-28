f250d7cdd0601e34da03aa8567494bc560ac26ab
141616 <lanhanghui@gmail.com>
2018-08-28 22:24:16 +0800

    UPDATE UI

--- pages/pack/index.js
+++ pages/pack/index.js
@@ -3,7 +3,8 @@ const app = getApp()
 Page({
   data: {
     userInfo: '',
-    current: 'homepage'
+    current: 'homepage',
+    kinds: ['技术类', '文学类', '数学类', '科学类', '文史类', '其他']
   },
   onLoad: function () {
     // get user
